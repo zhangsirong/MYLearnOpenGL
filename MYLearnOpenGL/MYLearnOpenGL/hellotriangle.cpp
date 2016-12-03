9590185c274fdcb207fd321f47b77da88238dfbe
zsr <zsr@zsrs-iMac.local>
2016-12-03 16:03:46 +0800

    通过索引四个顶点画矩形

--- MYLearnOpenGL/MYLearnOpenGL/hellotriangle.cpp
+++ MYLearnOpenGL/MYLearnOpenGL/hellotriangle.cpp
@@ -84,24 +84,28 @@ int main()
 
     //画一个距形
     GLfloat vertices[] = {
-      // First triangle
-       0.5f,  0.5f, 0.0f,  // Top Right
-       0.5f, -0.5f, 0.0f, // Bottom Right
-      -0.5f,  0.5f, 0.0f, // Top Left
-      // Second triangle
-       0.5f, -0.5f, 0.0f, // Bottom Right
-      -0.5f, -0.5f, 0.0f,  // Bottom Left
-      -0.5f,  0.5f, 0.0f   // Top Left
+        0.5f,  0.5f, 0.0f,  // Top Right
+        0.5f, -0.5f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  // Bottom Left
+        -0.5f,  0.5f, 0.0f   // Top Left
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3,  // First Triangle
+        1, 2, 3   // Second Triangle
     };
     
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
     glGenBuffers(1, &VBO);
     
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     
@@ -118,13 +122,15 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glBindVertexArray(0);
         
         glfwSwapBuffers(window);
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
