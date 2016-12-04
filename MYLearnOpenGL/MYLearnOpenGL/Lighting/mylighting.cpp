0ee85bd308c8038423830526fdaa20a651d64616
zsr <zsr@zsrs-iMac.local>
2016-12-04 21:27:31 +0800

    光源旋转

--- MYLearnOpenGL/MYLearnOpenGL/Lighting/mylighting.cpp
+++ MYLearnOpenGL/MYLearnOpenGL/Lighting/mylighting.cpp
@@ -143,6 +143,9 @@ int main()
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+        lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
+        
         lightingShader.Use();
         GLint objectColorLoc = glGetUniformLocation(lightingShader.Program, "objectColor");
         GLint lightColorLoc  = glGetUniformLocation(lightingShader.Program, "lightColor");
